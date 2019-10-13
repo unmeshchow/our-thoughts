641cbdb882e1050a4240d864a675fb8dd4dde80d
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-10-13 18:27:44 +0600

    Initial commit - add project with all dependencies

---
+++ src/main/java/com/unmeshc/ourthoughts/OurThoughtsApplication.java
@@ -0,0 +1,13 @@
+package com.unmeshc.ourthoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurThoughtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurThoughtsApplication.class, args);
+    }
+
+}
