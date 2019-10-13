4ae964f3e3153eb86b99c3f2743d57388b25284b
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-10-13 22:28:40 +0600

    Added index controller. Closes #4

---
+++ src/main/java/com/unmeshc/ourthoughts/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.unmeshc.ourthoughts.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by uc on 10/13/2019
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
