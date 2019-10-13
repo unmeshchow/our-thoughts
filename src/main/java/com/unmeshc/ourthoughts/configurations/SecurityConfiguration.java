bb772c51440d1d274957168ff58f4e07cb7f7806
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-10-13 20:53:17 +0600

    Added common fragments. Closes #1

---
+++ src/main/java/com/unmeshc/ourthoughts/configurations/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.unmeshc.ourthoughts.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by uc on 10/13/2019
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+           .antMatchers("/h2-console/**");
+    }
+}
---
+++ src/main/java/com/unmeshc/ourthoughts/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.unmeshc.ourthoughts.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by uc on 10/13/2019
+ */
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
