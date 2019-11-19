1aabeba533cd96ea4a9c1db9c501ec4fbad95ea3
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-11-19 11:54:32 +0600

    Added @Transactional to the service methods that retrieve the image (lob)

--- src/main/java/com/unmeshc/ourthoughts/services/UserServiceImpl.java
+++ src/main/java/com/unmeshc/ourthoughts/services/UserServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -118,6 +119,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public byte[] getImageForUserById(long userId) {
         return serviceUtils.convertIntoByteArray(getUserById(userId).getImage());
     }
